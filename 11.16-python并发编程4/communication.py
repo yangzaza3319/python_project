ef9b09c2c55a690d334eaaac479674e9a2f4aa89
jeyng <ljy3192003@163.com>
2025-11-17 18:06:32 +0800

    管道和队列（无案例）

---
+++ 11.16-python并发编程4/communication.py
@@ -0,0 +1,26 @@
+from multiprocessing import Queue
+
+q = Queue(3)
+
+q.put('1')
+q.put('2')
+q.put('3')
+
+try:
+    q.put_nowait('4')
+except:
+    print("队列已经满了")
+
+print(q.get())
+print(q.get())
+print(q.get())
+
+# print(q.get()) # 此时队列已空，在运行这段代码，会发生阻塞——>解决方法 等待队列中put新数据
+
+
+try:
+    q.get_nowait()
+except:
+    print("队列已经空了")
+
+print(q.empty())
