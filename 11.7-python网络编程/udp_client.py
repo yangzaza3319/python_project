e03cf913ecf5fc887f1ff5e0908ca127a8ef4ece
jeyng <ljy3192003@163.com>
2026-04-14 10:24:20 +0800

    修改项目结构，重命名一些文件

--- 11.7-python网络编程/udp_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.sendto(b"hello UDP",('localhost',8888))
-response,_ = client.recvfrom(1024)
-print("server response:",response.decode())
-
-client.close()
